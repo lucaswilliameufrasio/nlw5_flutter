98cdbc7fda826f3afbdc918fe65bef70093b503f
Lucas William <lucaswilliameufrasio@gmail.com>
2021-04-19 10:46:51 -0300

    feat: add HomePage widget

---
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -0,0 +1,53 @@
+import 'package:DevQuiz/core/core.dart';
+import 'package:DevQuiz/home/widgets/score_card/score_card_widget.dart';
+import 'package:flutter/material.dart';
+
+class AppBarWidget extends PreferredSize {
+  AppBarWidget()
+      : super(
+          preferredSize: Size.fromHeight(250),
+          child: Container(
+            height: 250,
+            child: Stack(
+              children: [
+                Container(
+                  height: 161,
+                  width: double.maxFinite,
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  decoration: BoxDecoration(
+                    gradient: AppGradients.linear,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text.rich(
+                        TextSpan(
+                            text: "Olá, ",
+                            style: AppTextStyles.title,
+                            children: [
+                              TextSpan(
+                                text: "Lucas William",
+                                style: AppTextStyles.titleBold,
+                              )
+                            ]),
+                      ),
+                      Container(
+                        width: 58,
+                        height: 58,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: NetworkImage(
+                                "https://github.com/lucaswilliameufrasio.png"),
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Align(alignment: Alignment(0.0, 1.0), child: ScoreCardWidget()),
+              ],
+            ),
+          ),
+        );
+}
---
+++ lib/home/widgets/score_card/score_card_widget.dart
@@ -0,0 +1,52 @@
+import 'package:DevQuiz/core/core.dart';
+import 'package:DevQuiz/home/widgets/chart/chart_widget.dart';
+import 'package:flutter/material.dart';
+
+class ScoreCardWidget extends StatelessWidget {
+  ScoreCardWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 14.0),
+      child: Container(
+        height: 136,
+        decoration: BoxDecoration(
+          color: AppColors.white,
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        padding: EdgeInsets.only(left: 16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 1,
+              child: ChartWidget(),
+            ),
+            Expanded(
+              flex: 3,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 24.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Vamos começar",
+                      style: AppTextStyles.heading,
+                    ),
+                    Text(
+                      "Complete os defasios e avance em conhecimento",
+                      style: AppTextStyles.body,
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
